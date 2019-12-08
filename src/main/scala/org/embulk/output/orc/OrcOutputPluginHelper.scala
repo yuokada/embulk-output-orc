c1c768529f444d8721845dcdd21a1d3a02a7a662
Yukihiro Okada <callistoiv+git@gmail.com>
2019-12-08 17:11:09 +0900

    Add type annotation

--- src/main/scala/org/embulk/output/orc/OrcOutputPluginHelper.scala
+++ src/main/scala/org/embulk/output/orc/OrcOutputPluginHelper.scala
@@ -35,8 +35,8 @@ object OrcOutputPluginHelper {
     case _ => false
   }
 
-  def getSchema(fpath: String) = {
-    val schema = fpath.split("://").toList.apply(0)
+  def getSchema(fpath: String): String = {
+    val schema = fpath.split("://").toList.head
     schema match {
       case "s3" | "s3a" | "s3n" => schema
       case _ => {
@@ -46,7 +46,7 @@ object OrcOutputPluginHelper {
     }
   }
 
-  def parseS3Url(s3url: String) = {
+  def parseS3Url(s3url: String): AmazonS3URILikeObject = {
     val parts = s3url.split("(://|/)").toList
     val bucket = parts.apply(1)
     val key = parts.slice(2, parts.size).mkString("/")
@@ -54,4 +54,5 @@ object OrcOutputPluginHelper {
   }
 
   case class AmazonS3URILikeObject(@BeanProperty bucket: String, @BeanProperty key: String)
+
 }
